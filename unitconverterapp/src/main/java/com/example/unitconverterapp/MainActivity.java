bd858fd794169076511619ab1ec4b07496a3a63c
Robert Beraru <robert_beraru@yahoo.com>
2025-09-28 20:44:49 +0300

    Final config changes for the Unit Converter App

--- unitconverterapp/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ unitconverterapp/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.unitconverterapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText title_text, insert_text;
+    TextView textView2, textView3;
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        title_text = findViewById(R.id.title_text);
+        insert_text = findViewById(R.id.insert_text);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        myButton = findViewById(R.id.myButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = insert_text.getText().toString();
+                double kilos = Double.parseDouble(inputText);
+
+                double pounds = makeConversion(kilos);
+                textView3.setText("" + pounds);
+            }
         });
+    }
+
+    public double makeConversion(double kilos){
+        //1 kilo = 2.20462
+        return kilos*2.20462;
     }
 }
